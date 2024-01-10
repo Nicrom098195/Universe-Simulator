8daeb556ff25de99328a8d1b0e69ac360272421d
Nicrom098195 <88785297+Nicrom098195@users.noreply.github.com>
2024-01-10 14:52:43 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -18,6 +18,7 @@ plength=8
 planets=max(0, min(40,planets))
 stars=max(0, min(20, stars))
 blackHoles=max(0, min(30, blackHoles))
+winSize=st["winSize"]
 
 print(planets,stars,blackHoles)
 
@@ -43,7 +44,7 @@ for i in range(stars):
     data[n]["col"]=(r,rin(0,255),255-r)
     data[n]["rad"]=rin(10,30)
     data[n]["grav"]=rin(20,50)
-    data[n]["pos"]=gpos(1600,900)
+    data[n]["pos"]=gpos(winSize[0],winSize[1])
     data[n]["vel"]=[0,0]
     data[n]["type"]=t
     data[n]["temp"]=rin(int(round(maxTemps[t]/3)),maxTemps[t])
@@ -55,7 +56,7 @@ for i in range(planets):
     data[n]["col"]=(rin(100,200),rin(100,200),0)
     data[n]["rad"]=rin(1,10)
     data[n]["grav"]=rin(1,20)
-    data[n]["pos"]=gpos(1600,900)
+    data[n]["pos"]=gpos(winSize[0],winSize[1])
     data[n]["vel"]=[rin(-2,2),rin(-2,2)]
     data[n]["type"]=rin(0,1)
     data[n]["temp"]=0
@@ -66,7 +67,7 @@ for i in range(blackHoles):
     data[n]["col"]=(10,10,10)
     data[n]["rad"]=rin(10,40)
     data[n]["grav"]=rin(1000,2000)
-    data[n]["pos"]=gpos(1600,900)
+    data[n]["pos"]=gpos(winSize[0],winSize[1])
     data[n]["vel"]=[rin(-2,2),rin(-2,2)]
     data[n]["type"]=4
     data[n]["temp"]=maxTemps[4]
@@ -78,3 +79,4 @@ with open(fname, "w") as f:
     f.write(json.dumps(data))
 with open("settings.json", "w") as f:
     f.write(json.dumps(st))
+
